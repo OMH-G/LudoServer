6f87701b4ad5048472cace83fadcfa3da97ef3b7
Omkar <omkarhalgi50@gmail.com>
2023-10-09 13:02:47 +0530

    asld

--- index.js
+++ index.js
@@ -9,19 +9,7 @@ const configSupabase = require('./configSupabase');
 const app = express();
 let allowedOrigins = ['https://ludokings.vercel.app'];
 
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
